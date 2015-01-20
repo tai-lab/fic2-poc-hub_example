5d2914a2d74e7d80efb6e57456430cc1e0bcd6a4
cgeoffroy <cgeoffroy@users.noreply.github.com>
2015-01-20 11:25:52 +0100

    Removing debug print

--- hub/views.py
+++ hub/views.py
@@ -1,4 +1,3 @@
-import pprint
 import uuid
 from hub import app
 from hub.user import User
@@ -13,20 +12,15 @@ def root():
 
 @app.route('/authorize')
 def authorize():
-    print('AA')
     resp = app.remote.authorized_response()
-    print('BB')
     if resp is None:
-        print('rrr')
         default = 'Unable the check authorization with remote'
         msg = 'reason=%s error=%s' % (
             request.args.get('error_reason', default), request.args.get('error_description', default))
         data = {'title': 'Access denied', 'msg': msg}
-        print('cc')
         return render_template('error.html', **data)
-    print(pprint.pprint(resp))
+    app.logger.info('OAuth response: %s', resp)
     d_user = {'access_token': resp['access_token'], 'refresh_token': resp['refresh_token'], 'id': str(uuid.uuid4())}
     session['remote_oauth'] = d_user
     login_user(User(d_user))
-    print('dd')
     return redirect(url_for('root', _scheme="https", _external=True))
